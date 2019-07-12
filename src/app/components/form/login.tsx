1e4c9f083ae975d4a1bc7a426ff0d36180b0a791
Bernice Ng Kai Ling <bernice.ngkl@axa.com.sg>
2019-07-12 15:31:07 +0800

    modified style of login page

--- src/app/components/form/login.tsx
+++ src/app/components/form/login.tsx
@@ -63,7 +63,28 @@ class Login extends React.Component<InitialProps, LoginState> {
   render() {
     return(
       <div className={styles.background}>
-        <div className={styles.left}>
+        <div className={styles.container}>
+          <div className={styles.logo}>
+            <div>
+              <img src={logo}/>
+            </div>
+            <div>
+              Bernice Ng <br/> Kai Ling
+            </div>
+          </div>
+          <div className={styles.title}>
+            Login with the credentials provided
+          </div>
+          <div className={styles.form}>
+            <Input label="Username" type="text" inputRef={this.usernameRef} placeholder="Enter Username Provided"/>
+            <Input label="Password" type="password" inputRef={this.passwordRef} placeholder="Enter Password Provided"/>
+            <Input label="Secret Key" type="password" inputRef={this.secretRef} placeholder="Enter Any Secret Key"/>
+            <div onClick={this.login}>
+              <div>Login</div>
+            </div>
+          </div>
+        </div>
+        {/* <div className={styles.left}>
           <div className={styles.title}>
             <div>
               "
@@ -74,29 +95,7 @@ class Login extends React.Component<InitialProps, LoginState> {
             </div>
           </div>
         </div>
-        <div className={styles.right}>
-          <div className={styles.container}>
-            <div className={styles.logo}>
-              <div>
-                <img src={logo}/>
-              </div>
-              <div>
-                Bernice Ng <br/> Kai Ling
-              </div>
-            </div>
-            <div className={styles.title}>
-              Login with the credentials provided
-            </div>
-            <div className={styles.form}>
-              <Input label="Username" type="text" inputRef={this.usernameRef} placeholder="Enter Username Provided"/>
-              <Input label="Password" type="password" inputRef={this.passwordRef} placeholder="Enter Password Provided"/>
-              <Input label="Secret Key" type="password" inputRef={this.secretRef} placeholder="Enter Any Secret Key"/>
-              <div onClick={this.login}>
-                <div>Login</div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <div className={styles.right}> */}
       </div>
     );
   }
