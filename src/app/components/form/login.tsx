e7aa9511ee29109a1e43064abb0d8c74b5a45540
NG KAI LING, BERNICE <bernice.ng.2016@smu.edu.sg>
2019-07-14 19:23:35 +0800

    added code to remove jwt token from localstorage when expired

--- src/app/components/form/login.tsx
+++ src/app/components/form/login.tsx
@@ -37,6 +37,7 @@ class Login extends React.Component<InitialProps, LoginState> {
     }).then((res) => {
       if (res.data.valid) {
         this.props.history.push('/aboutme');
+        localStorage.removeItem("resume-token");
       }
     });
   }
